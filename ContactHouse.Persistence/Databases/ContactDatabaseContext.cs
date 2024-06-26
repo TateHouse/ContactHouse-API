6387153ef1014c2aa23561fc6b37c86197642674
TateHouse <tatemonroehouse@gmail.com>
2024-06-26 17:28:06 -0400

    Feature: Contact database context

---
+++ ContactHouse.Persistence/Databases/ContactDatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿namespace ContactHouse.Persistence.Databases;
+using ContactHouse.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+public class ContactDatabaseContext : DbContext
+{
+	public virtual DbSet<Contact> Contacts { get; set; }
+
+	public ContactDatabaseContext(DbContextOptions<ContactDatabaseContext> databaseContextOptions)
+		: base(databaseContextOptions)
+	{
+
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+
+		var entityBuilder = modelBuilder.Entity<Contact>();
+		entityBuilder.HasKey(entity => entity.ContactId);
+	}
+}
